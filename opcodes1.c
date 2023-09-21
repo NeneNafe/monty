01a2f3b03af235fc4a1e940e9e9eefd825a2bb3e
NeneNafe <nenechiwaya@gmail.com>
2023-09-21 13:37:01 -0700

    everything

---
+++ opcodes1.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @stack: pointer to a node of the stack
+ * @line_number: line number of the opcode
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: pointer to top node
+ * @line_number: line number of the opcode
+ */
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "swap");
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = *stack;
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	temp->prev = NULL;
+	*stack = temp;
+}
